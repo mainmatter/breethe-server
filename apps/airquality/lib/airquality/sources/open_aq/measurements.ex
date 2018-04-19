e5005cd85f99bd84b457223083b2f9bd848f3ccc
niklaslong <niklas@long.ch>
2018-04-19 10:49:58 +0200

    refactor OpenAQ.Measurements

--- apps/airquality/lib/airquality/sources/open_aq/measurements.ex
+++ apps/airquality/lib/airquality/sources/open_aq/measurements.ex
@@ -1,30 +1,24 @@
 defmodule Airquality.Sources.OpenAQ.Measurements do
-  alias Airquality.Repo
-  alias Airquality.Data.{Measurement, Location}
+  alias Airquality.Data
 
   def get_latest(lat, lon) do
-    url =
-      "#{Application.get_env(:airquality, :open_aq_api_endpoint)}/latest?coordinates=#{lat},#{lon}"
+    result = query_open_aq(lat, lon)
+    measurements = result["measurements"]
 
-    {:ok, response} = HTTPoison.get(url)
-    data = Poison.decode!(response.body)
+    location = Data.get_location(lat, lon)
 
-    %{"results" => results} = data
+    Enum.each(measurements, fn measurement ->
+      params =
+        measurement
+        |> parse_measurement()
+        |> Map.put_new(:location_id, location.id)
+        |> Data.add_coordinates(lat, lon)
 
-    Enum.each(results, fn result ->
-      measurements = result["measurements"]
-
-      Enum.each(measurements, fn measurement ->
-        params = parse_measurement(measurement, lat, lon)
-        changeset = Measurement.changeset(%Measurement{}, params)
-        Repo.insert!(changeset)
-      end)
+      Data.create_measurement(params)
     end)
   end
 
-  def parse_measurement(measurement, lat, lon) do
-    location = Repo.get_by(Location, coordinates: %Geo.Point{coordinates: {lat, lon}, srid: 4326})
-
+  def parse_measurement(measurement) do
     %{
       "parameter" => parameter,
       "lastUpdated" => measured_at,
@@ -36,13 +30,20 @@ defmodule Airquality.Sources.OpenAQ.Measurements do
       parameter: parameter,
       measured_at: Timex.parse!(measured_at, "{ISO:Extended:Z}"),
       value: value,
-      unit: convert_unit(unit),
-      coordinates: %Geo.Point{coordinates: {lat, lon}, srid: 4326},
-      location_id: location.id
+      unit: convert_unit(unit)
     }
   end
 
-  def convert_unit(unit) do
+  defp query_open_aq(lat, lon) do
+    url =
+      "#{Application.get_env(:airquality, :open_aq_api_endpoint)}/latest?coordinates=#{lat},#{lon}"
+
+    {:ok, response} = HTTPoison.get(url)
+    %{"results" => [result]} = Poison.decode!(response.body)
+    result
+  end
+
+  defp convert_unit(unit) do
     case unit do
       "µg/m³" -> :micro_grams_m3
       "mg/m³" -> :milli_grams_m3
