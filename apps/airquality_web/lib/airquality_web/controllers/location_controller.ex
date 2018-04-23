c61eb8302d184183e1e619e6dbfcec6a0e1d90c4
niklaslong <niklas@long.ch>
2018-04-23 14:29:14 +0200

    support filter by lat/lon in Location endpoint

--- apps/airquality_web/lib/airquality_web/controllers/location_controller.ex
+++ apps/airquality_web/lib/airquality_web/controllers/location_controller.ex
@@ -1,11 +1,22 @@
 defmodule AirqualityWeb.LocationController do
   use AirqualityWeb, :controller
   alias Airquality.Sources.OpenAQ
-  require IEx
 
-  def index(conn, %{"filter" => %{"search" => search_term}}) do
-    locations = OpenAQ.get_locations(search_term)
+  def index(conn, %{"filter" => filter}) do
+    locations =
+      case process_params(filter) do
+        [lat, lon] -> OpenAQ.get_locations(lat, lon)
+        name -> OpenAQ.get_locations(name)
+      end
 
     render(conn, "index.json-api", data: locations)
+  end
+
+  defp process_params(%{"name" => name}), do: name
+
+  defp process_params(%{"coordinates" => coordinates}) do
+    coordinates
+    |> String.split(",")
+    |> Enum.map(&String.to_float/1)
   end
 end
