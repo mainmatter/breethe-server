e844bca4fd5c411de4dc27fbd3381144919487bc
niklaslong <niklas@long.ch>
2018-04-25 21:21:43 +0200

    add measurement controller test

---
+++ apps/airquality_web/test/airquality_web/controllers/measurement_controller_test.exs
@@ -0,0 +1,34 @@
+defmodule AirqualityWeb.MeasurementControllerTest do
+  use AirqualityWeb.ConnCase
+
+  import Mox
+  import Airquality.Factory
+
+  alias Airquality.Sources.OpenAQMock, as: Mock
+
+  setup :verify_on_exit!
+
+  describe "returns measurements" do
+    test "when filtering by location id" do
+      Mock
+      |> expect(:get_latest_measurements, fn _id -> build_list(1, :measurement) end)
+
+      conn = get(build_conn(), "api/measurements?filter[location]=1", [])
+
+      assert json_response(conn, 200) == %{
+               "data" => [
+                 %{
+                   "attributes" => %{
+                     "parameter" => "pm10",
+                     "unit" => "ppm",
+                     "value" => 13.2
+                   },
+                   "id" => "",
+                   "type" => "measurement"
+                 }
+               ],
+               "jsonapi" => %{"version" => "1.0"}
+             }
+    end
+  end
+end
