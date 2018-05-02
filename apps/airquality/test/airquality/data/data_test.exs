94c10beb20660088523191f87275bf2b92a89a3e
niklaslong <niklas@long.ch>
2018-05-02 19:15:58 +0200

    add Data tests

---
+++ apps/airquality/test/airquality/data/data_test.exs
@@ -0,0 +1,67 @@
+defmodule Airquality.DataTest do
+  use Airquality.DataCase
+
+  import Airquality.Factory
+
+  alias Airquality.{Data, Repo}
+  alias Airquality.Data.{Location, Measurement}
+
+  describe "location: " do
+    test "get_location by id" do
+      location = insert(:location)
+
+      assert location == Data.get_location(location.id)
+    end
+
+    test "get_location by params" do
+      location = insert(:location)
+
+      assert Repo.preload(location, :measurements) == Data.get_location(location)
+    end
+
+    test "create_location from params" do
+      params = params_for(:location)
+
+      location = Data.create_location(params)
+
+      assert Repo.get_by(Location, params) == location
+    end
+
+    test "create_location updates if location already exists" do
+      params = params_for(:location, last_updated: DateTime.utc_now())
+      location = insert(:location)
+
+      updated_location = Data.create_location(params)
+
+      assert params.last_updated == updated_location.last_updated
+      assert location.id == updated_location.id
+    end
+  end
+
+  describe "measurement: " do
+    test "get_measurement by params" do
+      params = params_for(:measurement)
+      location = insert(:location)
+      measurement = insert(:measurement, location_id: location.id)
+
+      assert measurement == Data.get_measurement(params)
+    end
+
+    test "create_measurement from params" do
+      location = insert(:location)
+      params = params_for(:measurement, location_id: location.id)
+
+      measurement = Data.create_measurement(params)
+
+      assert Repo.get_by(Measurement, params) == measurement
+    end
+
+    test "create_measurement updates (no-op) if measurement already exists" do
+      params = params_for(:measurement)
+      location = insert(:location)
+      measurement = insert(:measurement, location_id: location.id)
+
+      assert measurement == Data.create_measurement(params)
+    end
+  end
+end
