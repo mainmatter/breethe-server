5d989fd12fc15bb751365c1881d62a764bcf0676
niklaslong <niklas@long.ch>
2018-05-02 14:51:33 +0200

    create nil_measurements in controller

--- apps/airquality_web/lib/airquality_web/controllers/measurement_controller.ex
+++ apps/airquality_web/lib/airquality_web/controllers/measurement_controller.ex
@@ -2,10 +2,28 @@ defmodule AirqualityWeb.MeasurementController do
   use AirqualityWeb, :controller
 
   @source Application.get_env(:airquality, :source)
+  @parameters [:pm10, :pm25, :so2, :no2, :o3, :co, :bc]
 
   def index(conn, %{"filter" => %{"location" => location_id}}) do
     measurements = @source.get_latest_measurements(location_id)
+    nil_measurements = generate_missing_measurements(measurements)
 
-    render(conn, "index.json-api", data: measurements)
+    render(conn, "index.json-api", data: measurements ++ nil_measurements)
+  end
+
+  defp generate_missing_measurements(measurements) do
+    measurements
+    |> Enum.map(& &1.parameter)
+    |> (&(@parameters -- &1)).()
+    |> Enum.map(&add_nil_measurement/1)
+  end
+
+  defp add_nil_measurement(param) do
+    %{
+      parameter: param,
+      value: nil,
+      unit: nil,
+      measured_at: nil
+    }
   end
 end
