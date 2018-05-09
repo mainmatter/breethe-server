0b235cb1bf3d9371fcda10766feca4bb61232449
niklaslong <niklas@long.ch>
2018-05-09 10:00:12 +0200

    add repo to config/prod

--- apps/airquality/config/prod.exs
+++ apps/airquality/config/prod.exs
@@ -1,3 +1,7 @@
 use Mix.Config
 
-import_config "prod.secret.exs"
+config :airquality, Airquality.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
