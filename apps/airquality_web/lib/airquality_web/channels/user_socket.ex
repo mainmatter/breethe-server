331df1e127d0c3c102b9cb482d31cc3ba0b75307
niklaslong <niklas@long.ch>
2018-05-09 10:03:42 +0200

    add timeout to user_socket

--- apps/airquality_web/lib/airquality_web/channels/user_socket.ex
+++ apps/airquality_web/lib/airquality_web/channels/user_socket.ex
@@ -5,7 +5,7 @@ defmodule AirqualityWeb.UserSocket do
   # channel "room:*", AirqualityWeb.RoomChannel
 
   ## Transports
-  transport(:websocket, Phoenix.Transports.WebSocket)
+  transport(:websocket, Phoenix.Transports.WebSocket, timeout: 45_000)
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
