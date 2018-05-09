627b4a772811d2a2f22406d8de58bbcb4852af3c
niklaslong <niklas@long.ch>
2018-05-09 11:33:36 +0200

    remove channels

--- apps/airquality_web/lib/airquality_web.ex
+++ apps/airquality_web/lib/airquality_web.ex
@@ -49,13 +49,6 @@ defmodule AirqualityWeb do
     end
   end
 
-  def channel do
-    quote do
-      use Phoenix.Channel
-      import AirqualityWeb.Gettext
-    end
-  end
-
   @doc """
   When used, dispatch to the appropriate controller/view/etc.
   """
