06647cb281855742bece0d3f4e20030dc302e99d
niklaslong <niklas@long.ch>
2018-05-09 10:16:27 +0200

    add source to config/prod

--- apps/airquality/config/prod.exs
+++ apps/airquality/config/prod.exs
@@ -5,3 +5,5 @@ config :airquality, Airquality.Repo,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: true
+
+config :airquality, source: Airquality.Sources.OpenAQ
