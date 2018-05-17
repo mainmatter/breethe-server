446916d0e643d63996fc77b4842a30addfb79f37
niklaslong <niklas@long.ch>
2018-05-17 11:00:03 +0200

    split Task and cached locations tests

--- apps/airquality/test/airquality/airquality_test.exs
+++ apps/airquality/test/airquality/airquality_test.exs
@@ -21,7 +21,7 @@ defmodule AirqualityTest do
       assert [location] == Airquality.search_locations("pdx")
     end
 
-    test "returns locations and starts a background task to get locations from the API if 10 or more are present in the DB" do
+    test "returns cached locations if 10 or more are present in DB" do
       cached_locations = insert_list(10, :location, %{city: "pdx"})
 
       Mock
@@ -30,8 +30,21 @@ defmodule AirqualityTest do
 
       locations = Airquality.search_locations("pdx")
 
-      assert_tasks()
+      stop_tasks()
+
       assert locations == cached_locations
+    end
+
+    test "starts a background task to get locations from the API if 10 or more are present in the DB" do
+      cached_locations = insert_list(10, :location, %{city: "pdx"})
+
+      Mock
+      |> expect(:get_locations, fn _search_term -> Mock.get_locations(0.0, 0.0) end)
+      |> expect(:get_locations, fn _lat, _lon -> [cached_locations | insert_pair(:location)] end)
+
+      Airquality.search_locations("pdx")
+
+      assert_tasks_started()
     end
   end
 
@@ -45,7 +58,7 @@ defmodule AirqualityTest do
       assert [location] == Airquality.search_locations(0.0, 0.0)
     end
 
-    test "returns locations and starts a background task to get locations from the API if some are present in the DB" do
+    test "returns cached locations if 10 or more are present in DB" do
       lat = 0.0
       lon = 0.0
 
@@ -57,17 +70,42 @@ defmodule AirqualityTest do
 
       locations = Airquality.search_locations(lat, lon)
 
-      assert_tasks()
+      stop_tasks()
+
       assert locations == cached_locations
+    end
+
+    test "starts a background task to get locations from the API if 10 or more are present in the DB" do
+      lat = 0.0
+      lon = 0.0
+
+      cached_locations =
+        insert_list(10, :location, %{coordinates: %Geo.Point{coordinates: {lat, lon}, srid: 4326}})
+
+      Mock
+      |> expect(:get_locations, fn _lat, _lon -> [cached_locations | insert_pair(:location)] end)
+
+      Airquality.search_locations(lat, lon)
+
+      assert_tasks_started()
     end
   end
 
-  defp assert_tasks() do
+  defp assert_tasks_started() do
     tasks = Task.Supervisor.children(TaskSupervisor)
 
     Enum.all?(tasks, fn task ->
       ref = Process.monitor(task)
       assert_receive {:DOWN, ^ref, :process, _, :normal}, 500
+    end)
+  end
+
+  # stops background tasks to avoid throwing error when test process exits
+  defp stop_tasks() do
+    tasks = Task.Supervisor.children(TaskSupervisor)
+
+    Enum.each(tasks, fn task ->
+      Task.Supervisor.terminate_child(TaskSupervisor, task)
     end)
   end
 end
