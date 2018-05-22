fdfbb2d46a67dc7999aecc7fd3165d2e47538f7a
niklaslong <niklas@long.ch>
2018-05-22 10:55:45 +0200

    test for included measurements in location payload

--- apps/airquality_web/test/airquality_web/controllers/location_controller_test.exs
+++ apps/airquality_web/test/airquality_web/controllers/location_controller_test.exs
@@ -72,6 +72,60 @@ defmodule AirqualityWeb.LocationControllerTest do
                "jsonapi" => %{"version" => "1.0"}
              }
     end
+
+    test "and includes measurements in payload" do
+      location = insert(:location)
+      measurement = insert(:measurement, location_id: location.id)
+
+      Mock
+      |> expect(:search_locations, fn _search_term ->
+        [%{location | measurements: measurement}]
+      end)
+
+      conn = get(build_conn(), "api/locations?filter[name]=London", [])
+
+      assert json_response(conn, 200) == %{
+               "data" => [
+                 %{
+                   "attributes" => %{
+                     "name" => location.identifier,
+                     "city" => "test-city",
+                     "coordinates" => [10.0, 20.0],
+                     "country" => "test-country",
+                     "last-updated" => "2019-01-01T00:00:00.000000Z"
+                   },
+                   "relationships" => %{
+                     "measurements" => %{
+                       "links" => %{"related" => "/locations/#{location.id}/measurements"},
+                       "data" => %{
+                         "id" => "#{measurement.id}",
+                         "type" => "measurement"
+                       }
+                     }
+                   },
+                   "id" => "#{location.id}",
+                   "type" => "location"
+                 }
+               ],
+               "included" => [
+                 %{
+                   "attributes" => %{
+                     "measured-at" => "2019-01-01T00:00:00.000000Z",
+                     "parameter" => "pm10",
+                     "quality-index" => "very_low",
+                     "unit" => "micro_grams_m3",
+                     "value" => 0.0
+                   },
+                   "id" => "#{measurement.id}",
+                   "relationships" => %{
+                     "location" => %{"data" => %{"id" => "#{location.id}", "type" => "location"}}
+                   },
+                   "type" => "measurement"
+                 }
+               ],
+               "jsonapi" => %{"version" => "1.0"}
+             }
+    end
   end
 
   describe "show route: returns location" do
@@ -101,6 +155,56 @@ defmodule AirqualityWeb.LocationControllerTest do
                  "id" => "#{location.id}",
                  "type" => "location"
                },
+               "jsonapi" => %{"version" => "1.0"}
+             }
+    end
+
+    test "and includes measurements in payload" do
+      location = insert(:location)
+      measurement = insert(:measurement, location_id: location.id)
+
+      Mock
+      |> expect(:get_location, fn _location_id -> %{location | measurements: measurement} end)
+
+      conn = get(build_conn(), "api/locations/#{location.id}", [])
+
+      assert json_response(conn, 200) == %{
+               "data" => %{
+                 "attributes" => %{
+                   "name" => location.identifier,
+                   "city" => "test-city",
+                   "coordinates" => [10.0, 20.0],
+                   "country" => "test-country",
+                   "last-updated" => "2019-01-01T00:00:00.000000Z"
+                 },
+                 "relationships" => %{
+                   "measurements" => %{
+                     "links" => %{"related" => "/locations/#{location.id}/measurements"},
+                     "data" => %{
+                       "id" => "#{measurement.id}",
+                       "type" => "measurement"
+                     }
+                   }
+                 },
+                 "id" => "#{location.id}",
+                 "type" => "location"
+               },
+               "included" => [
+                 %{
+                   "attributes" => %{
+                     "measured-at" => "2019-01-01T00:00:00.000000Z",
+                     "parameter" => "pm10",
+                     "quality-index" => "very_low",
+                     "unit" => "micro_grams_m3",
+                     "value" => 0.0
+                   },
+                   "id" => "#{measurement.id}",
+                   "relationships" => %{
+                     "location" => %{"data" => %{"id" => "#{location.id}", "type" => "location"}}
+                   },
+                   "type" => "measurement"
+                 }
+               ],
                "jsonapi" => %{"version" => "1.0"}
              }
     end
