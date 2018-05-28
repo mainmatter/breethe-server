ec2d8e3508886bb365884ad0e3b13bbad1c53d97
niklaslong <niklas@long.ch>
2018-05-28 12:08:38 +0200

    add find_location/2 to Geocoding

--- apps/airquality/lib/airquality/sources/google/geocoding.ex
+++ apps/airquality/lib/airquality/sources/google/geocoding.ex
@@ -6,11 +6,34 @@ defmodule Airquality.Sources.Google.Geocoding do
         "key" => Application.get_env(:airquality, :google_maps_api_key)
       })
 
+    query
+    |> query_google_api()
+    |> Poison.decode!()
+    |> strip()
+  end
+
+  def find_location(lat, lon) do
+    query =
+      URI.encode_query(%{
+        "latlng" => "#{lat},#{lon}",
+        "key" => Application.get_env(:airquality, :google_maps_api_key),
+        "result_type" => "street_address"
+      })
+
+    query
+    |> query_google_api()
+    |> Poison.decode!()
+    |> strip()
+  end
+
+  defp query_google_api(query) do
     url = "#{Application.get_env(:airquality, :google_maps_api_endpoint)}?#{query}"
     {:ok, response} = HTTPoison.get(url)
-
-    data = Poison.decode!(response.body)
-    %{"results" => [%{"geometry" => %{"location" => %{"lat" => lat, "lng" => lon}}}]} = data
-    [lat, lon]
+    response.body
   end
+
+  defp strip(%{"results" => [%{"geometry" => %{"location" => %{"lat" => lat, "lng" => lon}}}]}),
+    do: [lat, lon]
+
+  defp strip(%{"results" => [%{"formatted_address" => address}]}), do: address
 end
