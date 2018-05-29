2e4db385c04e186914b72811c917626644312e4a
niklaslong <niklas@long.ch>
2018-05-29 20:15:21 +0200

    remove static manifest from config/prod.exs

--- apps/airquality_web/config/prod.exs
+++ apps/airquality_web/config/prod.exs
@@ -17,7 +17,6 @@ config :airquality_web, AirqualityWeb.Endpoint,
   load_from_system_env: true,
   url: [scheme: "https", host: "ppm-server.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # ## SSL Support
