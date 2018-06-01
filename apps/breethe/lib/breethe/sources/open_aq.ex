1c755711d1abf8e206b01f92569517b6e43d46f6
niklaslong <niklas@long.ch>
2018-06-01 10:05:25 +0200

    remove outdated documentation

--- apps/breethe/lib/breethe/sources/open_aq.ex
+++ apps/breethe/lib/breethe/sources/open_aq.ex
@@ -5,15 +5,8 @@ defmodule Breethe.Sources.OpenAQ do
   alias Breethe.Sources.{Google, OpenAQ}
 
   defmodule Behaviour do
-    @moduledoc """
-    Defines the behaviour for all implementations of Sources (Sources.HTTPClient, Sources.InMemory, ...)
-    """
-
-    @doc "returns a list of locations based on search term"
     @callback get_locations(search_term :: String.t()) :: [%Breethe.Data.Location{}]
-    @doc "returns a list of locations based on coordinates (lat/lon)"
     @callback get_locations(lat :: number, lon :: number) :: [%Breethe.Data.Location{}]
-    @doc "returns a list of measurements based on location id"
     @callback get_latest_measurements(location_id :: integer | String.t()) :: [
                 %Breethe.Data.Measurement{}
               ]
