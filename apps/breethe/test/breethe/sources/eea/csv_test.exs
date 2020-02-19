89ec991d79e89f16d0cb4cfd7375366c129bfe58
nkls <niklas@long.ch>
2020-02-19 09:34:27 +0100

    use heredoc syntax

--- apps/breethe/test/breethe/sources/eea/csv_test.exs
+++ apps/breethe/test/breethe/sources/eea/csv_test.exs
@@ -3,67 +3,19 @@ defmodule Breethe.Sources.EEA.CSVTest do
 
   alias Breethe.Sources.EEA.CSV
 
-  @leading [
-    "network_countrycode",
-    "network_localid",
-    "network_name",
-    "network_namespace",
-    "network_timezone",
-    "pollutant",
-    "samplingpoint_localid",
-    "samplingpoint_namespace",
-    "samplingpoint_x",
-    "samplingpoint_y",
-    "coordsys",
-    "station_code",
-    "station_localid",
-    "station_name",
-    "station_namespace",
-    "value_datetime_begin",
-    "value_datetime_end",
-    "value_datetime_inserted",
-    "value_datetime_updated",
-    "value_numeric",
-    "value_validity",
-    "value_verification",
-    "station_altitude",
-    "value_unit\r\nGB",
-    "NET-GB001A",
-    "National air network",
-    "GB.GB-ExEA.AQ",
-    "http://dd.eionet.europa.eu/vocabulary/aq/timezone/UTC+02",
-    "SO2",
-    "SPO-BG0071A_00001_100",
-    "GB.GB-ExEA.AQ",
-    "00.72",
-    "34.65975799999999",
-    "EPSG:4979",
-    "GB0071A",
-    "STA-GB0071A",
-    "London",
-    "GB.GB-ExEA.AQ",
-    "2019-11-10 23:00:00+01:00",
-    "2019-11-11 00:00:00+01:00",
-    "2019-11-11 01:29:21+01:00",
-    "+01:00"
-  ]
+  @sample_valid_data """
+  network_countrycode,network_localid,network_name,network_namespace,network_timezone,pollutant,samplingpoint_localid,samplingpoint_namespace,samplingpoint_x,samplingpoint_y,coordsys,station_code,station_localid,station_name,station_namespace,value_datetime_begin,value_datetime_end,value_datetime_inserted,value_datetime_updated,value_numeric,value_validity,value_verification,station_altitude,value_unit\r
+  GB,NET-GB001A,National air network,GB.GB-ExEA.AQ,http://dd.eionet.europa.eu/vocabulary/aq/timezone/UTC+02,SO2,SPO-BG0071A_00001_100,GB.GB-ExEA.AQ,00.72,34.65975799999999,EPSG:4979,GB0071A,STA-GB0071A,London,GB.GB-ExEA.AQ,2019-11-10 23:00:00+01:00,2019-11-11 00:00:00+01:00,2019-11-11 01:29:21+01:00,+01:00,3,-1,1,27,ug/m3\r
+  """
 
-  @trailing [
-    "-1",
-    "1",
-    "27",
-    "ug/m3\r\n"
-  ]
-
-  @valid_value_numeric "3"
-  @invalid_value_numeric ""
+  @sample_invalid_data """
+  network_countrycode,network_localid,network_name,network_namespace,network_timezone,pollutant,samplingpoint_localid,samplingpoint_namespace,samplingpoint_x,samplingpoint_y,coordsys,station_code,station_localid,station_name,station_namespace,value_datetime_begin,value_datetime_end,value_datetime_inserted,value_datetime_updated,value_numeric,value_validity,value_verification,station_altitude,value_unit\r
+  GB,NET-GB001A,National air network,GB.GB-ExEA.AQ,http://dd.eionet.europa.eu/vocabulary/aq/timezone/UTC+02,SO2,SPO-BG0071A_00001_100,GB.GB-ExEA.AQ,00.72,34.65975799999999,EPSG:4979,GB0071A,STA-GB0071A,London,GB.GB-ExEA.AQ,2019-11-10 23:00:00+01:00,2019-11-11 00:00:00+01:00,2019-11-11 01:29:21+01:00,+01:00,,-1,1,27,ug/m3\r
+  """
 
   describe "process_data(data): " do
     test "decodes and stores csv data in db" do
-      true
-      |> concat_data()
-      |> CSV.process_data()
-
+      CSV.process_data(@sample_valid_data)
       locations = Breethe.Data.all_locations()
 
       assert [
@@ -83,26 +35,10 @@ defmodule Breethe.Sources.EEA.CSVTest do
     end
 
     test "filters out data with no value" do
-      false
-      |> concat_data()
-      |> CSV.process_data()
-
+      CSV.process_data(@sample_invalid_data)
       locations = Breethe.Data.all_locations()
 
       assert [] = locations
-    end
-
-    defp concat_data(valid?) do
-      h = Enum.join(@leading, ",")
-      t = Enum.join(@trailing, ",")
-
-      m =
-        case valid? do
-          true -> @valid_value_numeric
-          false -> @invalid_value_numeric
-        end
-
-      h <> "," <> m <> "," <> t
     end
   end
 end
