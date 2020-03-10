8e73fa0fabb0e9b8e0cf4be556c127e9ab40e400
nkls <niklas@long.ch>
2020-03-10 16:47:56 +0100

    implement mix task for syncing data

--- apps/breethe/lib/mix/tasks/sync_data.ex
+++ apps/breethe/lib/mix/tasks/sync_data.ex
@@ -1,11 +1,13 @@
-defmodule Mix.Tasks.SyncMeasurements do
+defmodule Mix.Tasks.SyncData do
   use Mix.Task
 
-  alias Breethe.{Sources.EEA, Data}
+  alias Breethe.Sources.EEA
 
   @shortdoc "Refreshes European data"
 
+  @impl Mix.Task
   def run(_args) do
     Mix.Task.run("app.start")
+    EEA.get_data()
   end
 end
