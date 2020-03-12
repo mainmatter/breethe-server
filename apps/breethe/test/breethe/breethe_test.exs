1612b4b590700dbffb7d9540536803396036039d
nkls <niklas@long.ch>
2020-03-12 14:37:42 +0100

    update BreethTest

--- apps/breethe/test/breethe/breethe_test.exs
+++ apps/breethe/test/breethe/breethe_test.exs
@@ -1,40 +1,45 @@
 defmodule BreetheTest do
   use Breethe.DataCase
 
-  import Mox
   import Breethe.Factory
 
-  alias Breethe.TaskSupervisor
+  describe "get_location(location_id):" do
+    test "returns a location by id" do
+      location = insert(:location, measurements: [])
 
-  setup :set_mox_global
-  setup :verify_on_exit!
-
-  describe "get_location(location_id)" do
+      assert location == Breethe.get_location(location.id)
+    end
   end
 
   describe "search_locations(search_term):" do
+    test "returns locations for a search term" do
+      location = insert(:location, city: "london", measurements: [])
+
+      assert [location] == Breethe.search_locations("london")
+    end
   end
 
   describe "search_locations(lat, lon):" do
+    test "returns locations for latitude and longitude" do
+      lat = 0.0
+      lon = 0.0
+
+      location =
+        insert(:location,
+          coordinates: %Geo.Point{coordinates: {lat, lon}, srid: 4326},
+          measurements: []
+        )
+
+      assert [location] == Breethe.search_locations(lat, lon)
+    end
   end
 
   describe "search_measurements(location_id):" do
-  end
+    test "returns measurements for a location" do
+      location = insert(:location)
+      cached_measurement = [insert(:measurement, location_id: location.id)]
 
-  defp assert_background_tasks_started() do
-    TaskSupervisor
-    |> Task.Supervisor.children()
-    |> Enum.all?(fn task ->
-      ref = Process.monitor(task)
-      assert_receive {:DOWN, ^ref, :process, _, :normal}, 500
-    end)
-  end
-
-  defp stop_background_tasks() do
-    TaskSupervisor
-    |> Task.Supervisor.children()
-    |> Enum.each(fn task ->
-      Task.Supervisor.terminate_child(TaskSupervisor, task)
-    end)
+      assert cached_measurement == Breethe.search_measurements(location.id)
+    end
   end
 end
